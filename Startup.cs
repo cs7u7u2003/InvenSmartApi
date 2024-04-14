3c82930df034ec4341f051ea79898b7b2a3f958b
Jesus R <jesus.tortorella@gmail.com>
2024-04-14 15:20:08 -0500

    Implementar Swagger y Startup add Config data base

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // Este método se utiliza para configurar los servicios que la aplicación va a utilizar.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Aquí se agregan los servicios necesarios para el funcionamiento de la aplicación.
+        services.AddControllersWithViews();
+    }
+
+    // Este método se utiliza para configurar el pipeline de middleware de la aplicación.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
